48fdd1d4e764f778bf36ac7570ace8134eb274c0
marcogulino-mg <marcogulino98@gmail.com>
2025-10-30 17:21:16 +0100

    feat: add Footer component - Added Footer.jsx with social icons placeholder

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Social 1</a>
+          </li>
+          <li>
+            <a href="#">Social 2</a>
+          </li>
+          <li>
+            <a href="#">Social 3</a>
+          </li>
+          <li>
+            <a href="#">Social 4</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
